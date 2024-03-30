4faaa537b0cc5265b802b25fbe9341ea19454e50
Bill Hu <hza2002@foxmail.com>
2024-03-31 01:10:30 +0800

    Add ignore file, use dotenv to maAdd ignore file, utilize dotenv for managing private information, and format all Python files

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import gradio as gr
+from dotenv import dotenv_values
 from face_compare import FaceCompare
 from face_feature_analysis import FaceFeature
 
-appid = 'b6673a3a'
-api_secret = 'YzZhZDE3ZDI3Mjc4ZDI2NzJjMWMxZDU5'
-api_key = 'f65a0459f9e14b1faa052952376a3f5d'
+cfg = dotenv_values(".env")
 
-
-with gr.Blocks() as demo:
+with gr.Blocks(theme="nuttea/Softblue", title="智能人像分析器") as demo:
+    gr.Markdown(
+        """
+        # 智能人像分析器
+        > 基于讯飞开放平台开发
+        """
+    )
     with gr.Tab("人脸比对"):
         with gr.Row():
             pic1 = gr.Image(label="图片1", type="filepath")
@@ -20,16 +24,20 @@ with gr.Blocks() as demo:
                 res = f"可信度: {data['score']}\n结果: {data['desc']}"
                 return res
 
-            @compare.click(inputs=[pic1,pic2], outputs=result)
+            @compare.click(inputs=[pic1, pic2], outputs=result)
             def face_compare(img1_path, img2_path):
-                res = FaceCompare(appid, api_secret, api_key, img1_path, img2_path).run()
+                appid = cfg["FACE_COMPARE_APPID"]
+                api_secret = cfg["FACE_COMPARE_APISECRET"]
+                api_key = cfg["FACE_COMPARE_APIKEY"]
+                res = FaceCompare(
+                    appid, api_secret, api_key, img1_path, img2_path
+                ).run()
                 res = display_compare(res)
                 return res
 
-
     with gr.Tab("人脸特征分析"):
         with gr.Row():
-            img = gr.Image(label="分析图片", type='filepath')
+            img = gr.Image(label="分析图片", type="filepath")
             result = gr.TextArea(label="分析结果")
         analyze = gr.Button(value="开始分析")
 
@@ -41,9 +49,11 @@ with gr.Blocks() as demo:
 
         @analyze.click(inputs=img, outputs=result)
         def face_analyze(img_path):
-            api_key = "8e5b3958f8e856863ca8c43c2a0a2aab"
+            appid = cfg["FACE_ANALYZE_APPID"]
+            api_key = cfg["FACE_ANALYZE_APIKEY"]
             res = FaceFeature(appid, api_key, img_path).face_local_analysis()
             res = display_analyze(res)
             return res
+
 
 demo.launch()
--- face_feature_analysis.py
+++ face_feature_analysis.py
@@ -1,9 +1,9 @@
-# -*- coding: utf-8 -*-
 import requests
 import time
 import hashlib
 import base64
 import json
+
 
 class FaceDesc:
     def __init__(self, label):
@@ -12,89 +12,89 @@ class FaceDesc:
     def convert_age(self):
         age = int(self.label)
         if age == 0:
-            result = '0-1'
+            result = "0-1"
         elif age == 1:
-            result = '2-5'
+            result = "2-5"
         elif age == 2:
-            result = '6-10'
+            result = "6-10"
         elif age == 3:
-            result = '11-15'
+            result = "11-15"
         elif age == 4:
-            result = '16-20'
+            result = "16-20"
         elif age == 5:
-            result = '21-25'
+            result = "21-25"
         elif age == 6:
-            result = '31-40'
+            result = "31-40"
         elif age == 7:
-            result = '41-50'
+            result = "41-50"
         elif age == 8:
-            result = '51-60'
+            result = "51-60"
         elif age == 9:
-            result = '61-80'
+            result = "61-80"
         elif age == 10:
-            result = '80以上'
+            result = "80以上"
         elif age == 12:
-            result = '26-30'
+            result = "26-30"
         elif age == 11:
-            result = '其他'
+            result = "其他"
         else:
-            result = '图片文件有错误，或者格式不支持（gif图不支持）'
+            result = "图片文件有错误，或者格式不支持（gif图不支持）"
         return result
 
     def convert_score(self):
         score = int(self.label)
         if score == 0:
-            result = '漂亮'
+            result = "漂亮"
         elif score == 1:
-            result = '好看'
+            result = "好看"
         elif score == 2:
-            result = '普通'
+            result = "普通"
         elif score == 3:
-            result = '难看'
+            result = "难看"
         elif score == 4:
-            result = '其他'
+            result = "其他"
         elif score == 5:
-            result = '半人脸'
+            result = "半人脸"
         elif score == 6:
-            result = '多人'
+            result = "多人"
         else:
-            result = '图片文件有错误，或者格式不支持（gif图不支持）'
+            result = "图片文件有错误，或者格式不支持（gif图不支持）"
         return result
 
     def convert_sex(self):
         sex = int(self.label)
         if sex == 0:
-            result = '男人'
+            result = "男人"
         elif sex == 1:
-            result = '女人'
+            result = "女人"
         elif sex == 2:
-            result = '难以辨认'
+            result = "难以辨认"
         elif sex == 3:
-            result = '多人'
+            result = "多人"
         else:
-            result = '图片文件有错误，或者格式不支持（gif图不支持）'
+            result = "图片文件有错误，或者格式不支持（gif图不支持）"
         return result
 
     def convert_expression(self):
         exp = int(self.label)
         if exp == 0:
-            result = '其他(非人脸表情图片)'
+            result = "其他(非人脸表情图片)"
         elif exp == 1:
-            result = '其他表情'
+            result = "其他表情"
         elif exp == 2:
-            result = '喜悦'
+            result = "喜悦"
         elif exp == 3:
-            result = '愤怒'
+            result = "愤怒"
         elif exp == 4:
-            result = '悲伤'
+            result = "悲伤"
         elif exp == 5:
-            result = '惊恐'
+            result = "惊恐"
         elif exp == 6:
-            result = '厌恶'
+            result = "厌恶"
         elif exp == 7:
-            result = '中性  '
+            result = "中性  "
         else:
-            result = '图片文件有错误，或者格式不支持（gif图不支持）'
+            result = "图片文件有错误，或者格式不支持（gif图不支持）"
         return result
 
 
@@ -116,25 +116,31 @@ class FaceFeature:
 
         # 根据图片路径模式，构建param
         if self.mode == 0:  # 本地
-            param = "{\"image_name\":\"" + self.image_path + "\"}"
+            param = '{"image_name":"' + self.image_path + '"}'
         else:  # url
-            image_name = 'img.jpg'
-            param = "{\"image_name\":\"" + image_name + "\",\"image_url\":\"" + self.image_path + "\"}"
+            image_name = "img.jpg"
+            param = (
+                '{"image_name":"'
+                + image_name
+                + '","image_url":"'
+                + self.image_path
+                + '"}'
+            )
         # 构建头域中的X-Param
         curTime = str(int(time.time()))
         # 构建头域中的X-CheckSum
-        paramBase64 = base64.b64encode(param.encode('utf-8'))
+        paramBase64 = base64.b64encode(param.encode("utf-8"))
         # 构建头域中的X-CheckSum
-        tmp = str(paramBase64, 'utf-8')
+        tmp = str(paramBase64, "utf-8")
         m2 = hashlib.md5()
-        m2.update((self.API_KEY + curTime + tmp).encode('utf-8'))
+        m2.update((self.API_KEY + curTime + tmp).encode("utf-8"))
         checkSum = m2.hexdigest()
         # 头域
         header = {
-            'X-CurTime': curTime,
-            'X-Param': paramBase64,
-            'X-Appid': self.APPID,
-            'X-CheckSum': checkSum,
+            "X-CurTime": curTime,
+            "X-Param": paramBase64,
+            "X-Appid": self.APPID,
+            "X-CheckSum": checkSum,
         }
         return header
 
@@ -143,7 +149,7 @@ class FaceFeature:
         图片二进制数据
         :return:
         """
-        binfile = open(self.image_path, 'rb')
+        binfile = open(self.image_path, "rb")
         data = binfile.read()
         return data
 
@@ -158,14 +164,14 @@ class FaceFeature:
         try:
             result = requests.post(self.base_url + type, data=data, headers=headers)
             result = json.loads(result.content)
-            code = result['code']
+            code = result["code"]
             if code == 0:
-                label = result['data']['fileList'][0]['label']
+                label = result["data"]["fileList"][0]["label"]
             else:
-                label = result['desc']
+                label = result["desc"]
         except:
             code = -1
-            label = '校验类型%s是否正确' % type
+            label = "校验类型%s是否正确" % type
         return code, label
 
     def get_data(self):
@@ -181,17 +187,17 @@ class FaceFeature:
         # 调用__get_header方法完成头域的设置
         headers = self.__get_header()
         # 调用完成年龄接口的请求，接口type：age
-        code, age = self.__get_data_by_type('age', headers, data)
-        res.append({'type': 'age', 'code': code, 'value': age})
+        code, age = self.__get_data_by_type("age", headers, data)
+        res.append({"type": "age", "code": code, "value": age})
         # 调用完成颜值接口的请求，接口type：face_score
-        code, face_score = self.__get_data_by_type('face_score', headers, data)
-        res.append({'type': 'face_score', 'code': code, 'value': face_score})
+        code, face_score = self.__get_data_by_type("face_score", headers, data)
+        res.append({"type": "face_score", "code": code, "value": face_score})
         # 调用完成性别接口的请求，接口type：sex
-        code, sex = self.__get_data_by_type('sex', headers, data)
-        res.append({'type': 'sex', 'code': code, 'value': sex})
+        code, sex = self.__get_data_by_type("sex", headers, data)
+        res.append({"type": "sex", "code": code, "value": sex})
         # 调用完成表情接口的请求，接口type：expression
-        code, exp = self.__get_data_by_type('expression', headers, data)
-        res.append({'type': 'exp', 'code': code, 'value': exp})
+        code, exp = self.__get_data_by_type("expression", headers, data)
+        res.append({"type": "exp", "code": code, "value": exp})
         print(res)
         return res
 
@@ -204,47 +210,25 @@ class FaceFeature:
         """
         process_result = []
         for item in res:
-            if item['type'] == 'age':
-                if item['code'] == 0:  # 调用convert_age方法解析结果
-                    item['value'] = FaceDesc(item['value']).convert_age()
-                process_result.append({'type': '年龄', 'desc': item['value']})
-            elif item['type'] == 'face_score':
-                if item['code'] == 0:  # 调用convert_score方法解析结果
-                    item['value'] = FaceDesc(item['value']).convert_score()
-                    process_result.append({'type': '颜值', 'desc': item['value']})
-            elif item['type'] == 'sex':
-                if item['code'] == 0: # 调用convert_sex方法解析结果
-                    item['value'] = FaceDesc(item['value']).convert_sex()
-                process_result.append({'type': '性别', 'desc': item['value']})
+            if item["type"] == "age":
+                if item["code"] == 0:  # 调用convert_age方法解析结果
+                    item["value"] = FaceDesc(item["value"]).convert_age()
+                process_result.append({"type": "年龄", "desc": item["value"]})
+            elif item["type"] == "face_score":
+                if item["code"] == 0:  # 调用convert_score方法解析结果
+                    item["value"] = FaceDesc(item["value"]).convert_score()
+                    process_result.append({"type": "颜值", "desc": item["value"]})
+            elif item["type"] == "sex":
+                if item["code"] == 0:  # 调用convert_sex方法解析结果
+                    item["value"] = FaceDesc(item["value"]).convert_sex()
+                process_result.append({"type": "性别", "desc": item["value"]})
             else:
-                if item['code'] == 0: # 调用convert_expression方法解析结果
-                    item['value'] = FaceDesc(item['value']).convert_expression()
-                process_result.append({'type': '表情', 'desc': item['value']})
+                if item["code"] == 0:  # 调用convert_expression方法解析结果
+                    item["value"] = FaceDesc(item["value"]).convert_expression()
+                process_result.append({"type": "表情", "desc": item["value"]})
         return process_result
 
         return code, label
-
-    # def face_web_analysis(self):
-    #     """
-    #     网络图片：人脸特征分析
-    #     :return:
-    #     """
-    #     self.mode = 1
-    #     # 调用__get_header方法完成图的头域的设置
-    #     headers = self.__get_header()
-    #     code, age = self.__get_data('age', headers)
-    #     if code == 0:
-    #         age = FaceDesc(age).convert_age()
-    #     code, face_score = self.__get_data('face_score', headers)
-    #     if code == 0:
-    #         face_score = FaceDesc(face_score).convert_score()
-    #     code, sex = self.__get_data('sex', headers)
-    #     if code == 0:
-    #         sex = FaceDesc(sex).convert_sex()
-    #     code, exp = self.__get_data('expression', headers)
-    #     if code == 0:
-    #         exp = FaceDesc(exp).convert_expression()
-    #     # print(self.msg % (age, face_score, sex, exp))
 
     def face_local_analysis(self):
         """
@@ -257,13 +241,3 @@ class FaceFeature:
         # 调用process_data方法将数据进行解析
         process_data = self.process_data(request_data)
         return process_data
-
-
-if __name__ == '__main__':
-    APPID = "c66b9481"  # 应用ID
-    API_KEY = "8d47428b0ee220ee9c0c864690b4ec75"  # 接口密钥
-    print(r'..\\..\\static\\images\\upload\\feature.jpg')
-    res = FaceFeature(APPID, API_KEY, r'..\\..\\static\\images\\upload\\hand.jpg').face_local_analysis()
-    print(res)
-    # url = "http://hbimg.b0.upaiyun.com/a09289289df694cd6157f997ffa017cc44d4ca9e288fb-OehMYA_fw658"
-    # FaceFeature(url).face_web_analysis()
